edc1bf005d4b3769e040d25791dea64158b2f63f
bradygaster <bradyg@microsoft.com>
2025-04-28 14:35:45 -0700

    added client app

--- RandomFortuneApp.AppHost/Program.cs
+++ RandomFortuneApp.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Server>("server");
 
+builder.AddProject<Projects.Client>("client");
+
 builder.Build().Run();
